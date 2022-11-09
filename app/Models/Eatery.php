e6e6c6f4477db6fdaf15dc7442b4e8f90b0571d9
roghayeh shaygan <shaygan@gmail.com>
2022-11-09 07:35:02 +0330

    feat : add relationship

--- app/Models/Eatery.php
+++ app/Models/Eatery.php
@@ -4,11 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Eatery extends Model
 {
     use HasFactory;
     protected $guarded=['created_at','updated_at'];
 
+    public function foods(){
+        return $this->hasMany(Food::class);
+    }
 
+    public function seller()
+    {
+        return $this->belongsTo(Seller::class);
+    }
+
+    public function discounts(){
+        return $this->hasMany(Discount::class);
+    }
 }
--- app/Models/EateryType.php
+++ app/Models/EateryType.php
@@ -10,4 +10,13 @@ class EateryType extends Model
     use HasFactory;
     protected $table='eatery_types';
     protected $guarded=['created_at','updated_at'];
+
+    public function categories()
+    {
+        $this->hasMany(Category::class,'eatery_type_id','id');
+    }
+
+    public function eateries(){
+        $this->hasMany(Eatery::class,'type','id');
+    }
 }
