cb1fb05a4795cf6dba3413bcc6d0cf4006788e1b
roghayeh shaygan <shaygan@gmail.com>
2022-11-10 23:38:13 +0330

    feat : define user login method

--- app/Http/Controllers/user/AuthController.php
+++ app/Http/Controllers/user/AuthController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\user;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
+use http\Env\Response;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -16,6 +18,14 @@ class AuthController extends Controller
         return response([$user,$token],201);
     }
 
+    public function login(LoginRequest $request)
+    {
+        $user=User::where('email',$request->email)->where('password',$request->password)->first();
+        if(!$user)
+            return Response(['bad creds'],401);
+        $token=$user->createToken('userAuthToken')->plainTextToken;
+        return response([$user,$token],201);
+    }
 
     public function logout(){
         auth()->user()->tokens()->delete();
