9670042a71c34291dc031e058bf4b652a11b8346
roghayeh shaygan <shaygan@gmail.com>
2022-11-10 23:54:04 +0330

    fix : remove show action from seller's eatery resource

--- routes/seller.php
+++ routes/seller.php
@@ -9,7 +9,7 @@ Route::post('logout', [\App\Http\Controllers\Seller\AuthenticatedSessionControll
 Route::get('dashboard',\App\Http\Controllers\Seller\DashboardController::class)
     ->name('seller.dashboard');
 
-Route::resource('eateries',\App\Http\Controllers\EateryController::class,['as' => 'seller']);
+Route::resource('eateries',\App\Http\Controllers\EateryController::class,['as' => 'seller','except' => [ 'show' ]]);
 
 Route::resource('foods',\App\Http\Controllers\FoodController::class,['as'=>'seller']);
 
