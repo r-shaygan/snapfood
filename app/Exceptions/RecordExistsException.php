ca0ffac454750cca8cbb84adf7a02b593f8b9f07
roghayeh shaygan <shaygan@gmail.com>
2022-11-15 21:36:18 +0330

    feat: add exception to prevent storing repeated record in database

---
+++ app/Exceptions/RecordExistsException.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class RecordExistsException extends HttpException
+{
+    public function __construct(string $message = 'record is already exists', \Throwable $previous = null, int $code = 405, array $headers = [])
+    {
+        parent::__construct(405, $message, $previous, $headers, $code);
+    }
+
+    public function render($request)
+    {
+        return $this->shouldReturnJson($request)
+            ? $this->prepareJsonResponse($request)
+            : $this->prepareResponse($request);
+    }
+
+    protected function shouldReturnJson($request)
+    {
+        return $request->expectsJson();
+    }
+
+    protected function prepareJsonResponse($request)
+    {
+        return new JsonResponse(['message'=>$this->message],$this->code);
+    }
+
+    protected function prepareResponse($request)
+    {
+        return redirect()->intended()->withErrors($this->message);
+    }
+
+}
