ff3f898f726ed7a1f9493f928e3d0409ed48a146
roghayeh shaygan <shaygan@gmail.com>
2022-11-25 13:48:33 +0330

    feat: add new routes for user invoices and carts

--- routes/api-user.php
+++ routes/api-user.php
@@ -15,4 +15,13 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('addresses', \App\Http\Controllers\AddressController::class,['except'=>'show']);
     Route::get('addresses/{address}/default',[\App\Http\Controllers\AddressController::class,'setDefault']);
 
+    //cart
+    Route::apiResource('/carts',\App\Http\Controllers\CartController::class);
+    Route::post('/carts/{cart}/pay',[\App\Http\Controllers\CartController::class,'pay']);
+    Route::patch('/carts/{cart}',[\App\Http\Controllers\CartController::class,'update']);
+
+    //invoice
+    Route::get('/invoices',[\App\Http\Controllers\InvoiceController::class,'index']);
+    Route::get('/invoices/{invoice}',[\App\Http\Controllers\InvoiceController::class,'show']);
+
 });
