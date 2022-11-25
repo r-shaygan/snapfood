21109b416d452e7266dbf526122db3e36f906aa3
roghayeh shaygan <shaygan@gmail.com>
2022-11-25 17:25:31 +0330

    refactor: change DashboardController response

--- app/Http/Controllers/Seller/DashboardController.php
+++ app/Http/Controllers/Seller/DashboardController.php
@@ -15,6 +15,6 @@ class DashboardController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return view('seller.dashboard');
+        return redirect()->to(route('seller.invoices.index'));
     }
 }
